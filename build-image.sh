35b24b2d164bf625cd098a109391100a3cb2fa0e
SAD <zoulou31@free.fr>
2022-03-26 20:12:56 +0100

    dos2unix files

--- build-image.sh
+++ build-image.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-IMAGENAME=zabbixagentwebhook
 
-
-pipreqs ./
-
-#DOCKER_BUILDKIT=1 docker build -t $IMAGENAME .
-#docker rmi $(docker images -q) --force
-#docker builder prune -a -f && docker image prune -a -f && docker system prune -a -f
 docker image prune --filter "until=30m" -f
 docker container prune --filter "until=1h" -f
 docker network prune --filter "until=24h" -f
