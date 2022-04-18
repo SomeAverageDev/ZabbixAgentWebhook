67bccafad4d87321335721b027589d59017bc93c
SAD <zoulou31@free.fr>
2022-04-18 22:18:33 +0200

    docker build config update

--- build-image.sh
+++ build-image.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
+IMAGENAME=zabbixagentwebhook
 
+
+pipreqs ./
+
+#DOCKER_BUILDKIT=1 docker build -t $IMAGENAME .
+#docker rmi $(docker images -q) --force
+#docker builder prune -a -f && docker image prune -a -f && docker system prune -a -f
 docker image prune --filter "until=30m" -f
 docker container prune --filter "until=1h" -f
 docker network prune --filter "until=24h" -f
 docker builder prune --filter "until=1h" -f
 
 docker-compose -f docker-compose.prod.yml down -v
-DOCKER_BUILDKIT=1 docker-compose -f docker-compose.prod.yml build --no-rm
-DOCKER_BUILDKIT=1 docker-compose -f docker-compose.prod.yml up
+DOCKER_BUILDKIT=0 docker-compose -f docker-compose.prod.yml build --no-rm
+DOCKER_BUILDKIT=0 docker-compose -f docker-compose.prod.yml up
 
