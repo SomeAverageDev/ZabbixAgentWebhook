d8510f9f740be623855b749df2b8e9d6565d3cd3
SAD <zoulou31@free.fr>
2022-04-19 22:50:50 +0200

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -13,6 +13,6 @@ docker network prune --filter "until=24h" -f
 docker builder prune --filter "until=1h" -f
 
 docker-compose -f docker-compose.prod.yml down -v
-DOCKER_BUILDKIT=0 docker-compose -f docker-compose.prod.yml build --no-rm
-DOCKER_BUILDKIT=0 docker-compose -f docker-compose.prod.yml up
+DOCKER_BUILDKIT=1 docker-compose -f docker-compose.prod.yml build --no-rm
+DOCKER_BUILDKIT=1 docker-compose -f docker-compose.prod.yml up
 
