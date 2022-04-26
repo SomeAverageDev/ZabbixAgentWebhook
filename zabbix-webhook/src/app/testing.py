e1a34e749420ac747e3c65bb99d65766e487a97f
SAD <zoulou31@free.fr>
2022-04-26 21:55:38 +0200

    Update testing.py

--- zabbix-webhook/src/app/testing.py
+++ zabbix-webhook/src/app/testing.py
@@ -1,27 +1,89 @@
+"""
+Goal: Implement ZWL API testing
+@authors:
+    Gaël MONDON
+"""
+import base64
+import json
+
 from fastapi.testclient import TestClient
-from main import app
+from app.main import app
+from app.config import defaults
+
+
+valid_credentials = base64.b64encode(str.encode("{}:{}".format(defaults['username'], defaults['password']))).decode("utf-8")
 
 
 client = TestClient(app)
 
-json_data = {
-"Type":"Notification",
-"MessageId":"da41e39f-ea4d-435a-b922-c6aae3915ebe",
-"TopicArn":"arn:aws:sns:us-west-2:123456789012:MyTopic",
-"Subject":"test",
-"Message":"test message",
-"Timestamp":"2012-04-25T21:49:25.719Z",
-"SignatureVersion":"1",
-"Signature":"EXAMPLElDMXvB8r9R83tGoNn0ecwd5UjllzsvSvbItzfaMpN2nk5HVSw7XnOn/49IkxDKz8YrlH2qJXj2iZB0Zo2O71c4qQk1fMUDi3LGpij7RCW7AW9vYYsSqIKRnFS94ilu7NFhUzLiieYr4BKHpdTmdD6c0esKEYBpabxDSc=",
-"SigningCertURL":"https://sns.us-west-2.amazonaws.com/SimpleNotificationService-f3ecfb7224c7233fe7bb5f59f96de52f.pem",
-"UnsubscribeURL":"https://sns.us-west-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-west-2:123456789012:MyTopic:2bcfbf39-05c3-41de-beaa-fcfcc21c8f55"
-}
+def read_json_file(filename):
+    # read file
+    with open(filename, 'r') as file:
+        return file.read()
 
 
-def test_generic():
-    res = client.post("/zabbix/generic/", json=json_data)
-    assert res.status_code == 200
+
+def test_post_global(url, data):
+    return client.post(url, json=data, headers={"Content-Type": "application/json", "Authorization": "Basic " + valid_credentials})
+
+
+def test_post_aws(data):
+    response = test_post_global("/zabbix/aws/sns", data)
+    print('test_post_aws.status_code:{}'.format(response.status_code))
+    assert response.status_code == 200
+
+
+def test_post_gcp(data):
+    response = test_post_global("/zabbix/gcp", data)
+    print('test_post_gcp.status_code:{}'.format(response.status_code))
+    assert response.status_code == 200
+
+
+def test_post_azure_common(data):
+    response = test_post_global("/zabbix/azure/common", data)
+    print('test_post_azure_common.status_code:{}'.format(response.status_code))
+    assert response.status_code == 200
+
+
+def test_post_generic(data):
+    response = test_post_global("/zabbix/generic", data)
+    print('test_post_generic.status_code:{}'.format(response.status_code))
+    assert response.status_code == 200
+
+
+def test_get_health_stats():
+    response = client.get("/health/stats", headers={"Authorization": "Basic " + valid_credentials})
+    print('test_get_health_stats.status_code:{}'.format(response.status_code))
+    assert response.status_code == 200
+
+
+def test_get_health():
+    response = client.get("/health")
+    print('test_get_health.status_code:{}'.format(response.status_code))
+    assert response.status_code == 200
+
+
+def test_get_help():
+    response = client.get("/help")
+    print('test_get_help.status_code:{}'.format(response.status_code))
+    assert response.status_code == 200
 
 
 if __name__ == "__main__":
-    test_generic()
+    """ Health & stats """
+    test_get_help()
+    test_get_health()
+    test_get_health_stats()
+
+    """ Generic """
+    test_post_generic(read_json_file('../test/generic.json'))
+
+    """ Azure """
+    test_post_azure_common(read_json_file('../test/azure.common.json'))
+
+    """ GCP """
+    test_post_gcp(read_json_file('../test/gcp.incident1.json'))
+
+    """ AWS """
+    test_post_aws(read_json_file('../test/aws.notif.json'))
+
