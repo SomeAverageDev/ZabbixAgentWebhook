131b3570f5ea112024cc9d8a6f2d27ebf2d618fd
SAD <zoulou31@free.fr>
2022-04-27 22:46:31 +0200

    Update test_main.py

--- zabbix-webhook/src/app/tests/test_main.py
+++ zabbix-webhook/src/app/tests/test_main.py
@@ -5,6 +5,7 @@ Goal: Implement ZWL API testing
 """
 import base64
 import json
+import os
 
 from fastapi.testclient import TestClient
 from app.main import app
@@ -12,7 +13,7 @@ from app.config import defaults
 
 
 valid_credentials = base64.b64encode(str.encode("{}:{}".format(defaults['username'], defaults['password']))).decode("utf-8")
-
+current_dir = os.getcwd()
 
 client = TestClient(app)
 
@@ -27,55 +28,55 @@ def exec_post_global(url, data, add_headers=None):
     h = {"Content-Type": "application/json", "Authorization": "Basic " + valid_credentials}
     if add_headers is not None:
         h = h | add_headers
-    #print('headers:{}'.format(h))
+    #print(current_dir+'/headers:{}'.format(h))
     return client.post(url, json=data, headers=h)
 
 
 def test_post_aws():
     response = exec_post_global("/zabbix/aws/sns",
-                                read_json_file('app/tests/data/aws.notif.json'),
+                                read_json_file(current_dir+'/tests/data/aws.notif.json'),
                                 {'x-amz-sns-message-type': 'Notification'})
-    print('test_post_aws.status_code:{}'.format(response.status_code))
+    print(current_dir+'/test_post_aws.status_code:{}'.format(response.status_code))
     assert response.status_code == 200
 
 
 def test_post_gcp():
     response = exec_post_global("/zabbix/gcp",
-                                read_json_file('app/tests/data/gcp.incident1.json'))
-    print('test_post_gcp.status_code:{}'.format(response.status_code))
+                                read_json_file(current_dir+'/tests/data/gcp.incident1.json'))
+    print(current_dir+'/test_post_gcp.status_code:{}'.format(response.status_code))
     assert response.status_code == 200
 
 
 def test_post_azure_common():
     response = exec_post_global("/zabbix/azure/common",
-                                read_json_file('app/tests/data/azure.common.json'))
-    print('test_post_azure_common.status_code:{}'.format(response.status_code))
+                                read_json_file(current_dir+'/tests/data/azure.common.json'))
+    print(current_dir+'/test_post_azure_common.status_code:{}'.format(response.status_code))
     assert response.status_code == 200
 
 
 def test_post_generic():
     response = exec_post_global("/zabbix/generic",
-                                read_json_file('app/tests/data/generic.json'))
-    print('test_post_generic.status_code:{}'.format(response.status_code))
+                                read_json_file(current_dir+'/tests/data/generic.json'))
+    print(current_dir+'/test_post_generic.status_code:{}'.format(response.status_code))
     assert response.status_code == 200
 
 
 def test_get_health_stats():
     response = client.get("/health/stats",
                           headers={"Authorization": "Basic " + valid_credentials})
-    print('test_get_health_stats.status_code:{}'.format(response.status_code))
+    print(current_dir+'/test_get_health_stats.status_code:{}'.format(response.status_code))
     assert response.status_code == 200
 
 
 def test_get_health():
     response = client.get("/health")
-    print('test_get_health.status_code:{}'.format(response.status_code))
+    print(current_dir+'/test_get_health.status_code:{}'.format(response.status_code))
     assert response.status_code == 200
 
 
 def test_get_help():
     response = client.get("/help")
-    print('test_get_help.status_code:{}'.format(response.status_code))
+    print(current_dir+'/test_get_help.status_code:{}'.format(response.status_code))
     assert response.status_code == 200
 
 
